db9d12d0d032f3763f3307825ca262ab915793cf
cui <632381896@qq.com>
2020-07-27 16:12:10 +0800

    调整架构:chaos-worldpress

--- chaos-cocos/assets/script/Game.js
+++ chaos-cocos/assets/script/Game.js
@@ -2,55 +2,19 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        prefab6: {
-            default: null,
-            type: cc.Prefab
-        },
-        prefab7: {
-            default: null,
-            type: cc.Prefab
-        },
-        prefab8: {
-            default: null,
-            type: cc.Prefab
-        },
-        x:4,
-        y:3
-        
-    },
-
-    onLoad () {
-        for(var i=0;i<this.x;i++ ){
-            for(var j=0;j<this.y;j++ ){
-                var prefab;
-                if((i+j)%3==0){
-                    prefab = cc.instantiate(this.prefab6);
-                }else if((i+j)%3==1){
-                    prefab = cc.instantiate(this.prefab7);
-                }else if((i+j)%3==2){
-                    prefab = cc.instantiate(this.prefab8);
-                }
-                this.node.addChild(prefab);
-                prefab.setPosition(this.getPosition(i,j));
-                prefab.on('mousedown', function (event) {
-                    cc.tween(event.target)
-                    .to(1, { position: cc.v2(100, 100), rotation: 360 })
-                    .to(1, { scale: 2 })
-                    .start()
-                });
-            }
-        }
-        
-    },
-
-    start () {
 
     },
 
-    update (dt) {},
-    getPosition: function (x,y) {
-        var randX = -300+x*200;
-        var randY = -200+y*200;
-        return cc.v2(randX, randY);
+    onLoad() {
+
+
+
     },
+
+    start() {
+
+    },
+
+    update(dt) {},
+
 });
---
+++ chaos-mvvmhabit/app/src/main/java/com/cui/tech/mh/view/activity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.cui.tech.mh.view.activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.cui.tech.mh.BR;
+import com.cui.tech.mh.R;
+import com.cui.tech.mh.app.AppViewModelFactory;
+import com.cui.tech.mh.databinding.ActivityMainBinding;
+import com.cui.tech.mh.vm.MainViewModel;
+
+import java.util.List;
+
+import me.goldze.mvvmhabit.base.BaseActivity;
+
+/**
+ * @author G.G
+ * @date 2020/5/18 17:40
+ */
+public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewModel> {
+    private List<Fragment> mFragments;
+
+    @Override
+    public int initContentView(Bundle savedInstanceState) {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public int initVariableId() {
+        return BR.viewModel;
+    }
+
+    @Override
+    public MainViewModel initViewModel() {
+        AppViewModelFactory factory = AppViewModelFactory.getInstance(getApplication());
+        return ViewModelProviders.of(this, factory).get(MainViewModel.class);
+    }
+
+    @Override
+    public void initViewObservable() {
+    }
+
+    @Override
+    public void initData() {
+    }
+
+
+}
