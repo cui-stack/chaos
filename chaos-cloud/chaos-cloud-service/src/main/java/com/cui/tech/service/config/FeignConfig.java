9ac8f1079bb41b7d48e5a044d412b6a6a3c759a6
GG <632381896@qq.com>
2020-10-30 19:07:38 +0800

    增加chaos-cloud

---
+++ chaos-cloud/chaos-cloud-service/src/main/java/com/cui/tech/service/config/FeignConfig.java
@@ -0,0 +1,11 @@
+package com.cui.tech.service.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author G.G
+ * @date 2020/10/29 15:56
+ */
+@EnableFeignClients
+public class FeignConfig {
+}
---
+++ chaos-cloud/chaos-cloud-service/src/main/java/com/cui/tech/service/config/NacosConfig.java
@@ -0,0 +1,11 @@
+package com.cui.tech.service.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author G.G
+ * @date 2020/10/29 15:57
+ */
+@EnableDiscoveryClient
+public class NacosConfig {
+}
