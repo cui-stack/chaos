595c6c51cfe916eb0334216b4cdd7b10d0160540
阿暴  G.G <632381896@qq.com>
2021-04-24 16:57:15 +0800

    调整

--- chaos-weapp/app.js
+++ chaos-weapp/app.js
@@ -1,5 +1,7 @@
 import { reLaunch } from "/chaos/functions/Wx";
 const localHost = "http://127.0.0.1:8089";
+const devHost = "http://127.0.0.1:8089";
+const prodHost = "http://127.0.0.1:8089";
 
 App({
   onPageNotFound() {
